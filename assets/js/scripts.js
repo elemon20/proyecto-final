f721741816171e21bcb49fde19446091e91b58fd
Eleana <elemon20@hotmail.com>
2018-07-20 02:12:53 -0400

    add:css-and-js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,13 @@ console.log("hola")
 
 
 
+$(function() {
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      $("#toTop").addClass("show");
+    } else {
+      $("#toTop").removeClass("show");
+    }
+  });
+});
